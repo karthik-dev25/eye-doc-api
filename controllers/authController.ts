d92aab5863891ce272c33ed37f102d5fbe3347bf
karthikn25 <karthikn25.dev@gmail.com>
2025-02-02 17:36:23 +0530

    Auth API Completed

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,21 +1,30 @@
-import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { sendSignupEmail } from '../services/emailServices';
+// import { sendSignupEmail } from '../services/emailServices';
 import User from '../models/userModels';
+import {loginSchema, signupSchema} from '../utils/JoiValidation';
 
 const signup = async (req: any, res: any) => {
   try {
-    const { firstName, lastName, email, phone } = req.body;
+    // Validate request body
+    const { error } = signupSchema.validate(req.body, { abortEarly: false });
 
-    // Check if user already exists
+    if (error) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: error.details.map((err) => err.message),
+      });
+    }
+
+    const { firstName, lastName, email, phone, password } = req.body;
+
+    // Check if user already exists 
     const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
     if (existingUser) {
       return res.status(400).json({ message: 'Email or phone already exists' });
     }
 
     // Generate a random password
-    const password = Math.random().toString(36).slice(-8);
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create user
@@ -30,7 +39,8 @@ const signup = async (req: any, res: any) => {
     await newUser.save();
 
     // Send email with credentials
-    await sendSignupEmail(email, { firstName, lastName, email, phone, password });
+    // let emailRes = await sendSignupEmail(email, { firstName, lastName, email, phone, password });
+    // console.log('emailRes: ', emailRes);
 
     res.status(201).json({ message: 'User signed up successfully' });
   } catch (err:any) {
@@ -40,10 +50,21 @@ const signup = async (req: any, res: any) => {
 
 const login = async (req: any, res: any) => {
   try {
+     // Validate request body
+     const { error } = loginSchema.validate(req.body, { abortEarly: false });
+
+     if (error) {
+       return res.status(400).json({
+         message: "Validation failed",
+         errors: error.details.map((err) => err.message),
+       });
+     }
+
     const { email, phone, password } = req.body;
 
     // Find user by email or phone
-    const user = await User.findOne({ $or: [{ email }, { phone }] });
+    const user:any = await User.findOne({ $or: [{ email }, { phone }] });
+
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -58,8 +79,8 @@ const login = async (req: any, res: any) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
       expiresIn: '1h',
     });
-
-    res.status(200).json({ token });
+    
+    res.status(200).json({...user._doc, token });
   } catch (err:any) {
     res.status(500).json({ message: 'Error logging in', error: err.message });
   }
